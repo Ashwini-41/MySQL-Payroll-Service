9419852da8bbd1f36f160e04f6893967cc57b630
Ashwini Garad <ashwinigarad1041@gmail.com>
2024-09-09 23:45:02 +0530

    Insert data into table

--- PayrollService.sql
+++ PayrollService.sql
@@ -10,6 +10,11 @@ start Date NOT NULL,
 primary key (id)
 );
 describe employee_payroll;
+INSERT INTO employee_payroll (name,salary,start) VALUES
+('Raj',100000.0,'2024-01-03'),
+('Ashwini',200000.0,'2023-08-13'),
+('Tej', 300000.0,'2024-09-12'),
+('Shree' ,400000.0,'2023-07-11');
 
 
 
