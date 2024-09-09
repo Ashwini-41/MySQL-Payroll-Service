0be21ffe7689e27f7bf0c4584e486068f921fd85
Ashwini Garad <ashwinigarad1041@gmail.com>
2024-09-09 23:22:17 +0530

    employee_payroll table created

--- PayrollService.sql
+++ PayrollService.sql
@@ -2,4 +2,16 @@ create database payroll_service;
 use payroll_service;
 show databases;
 select database();
+create table employee_payroll(
+id INT unsigned NOT NULL auto_increment,
+name VARCHAR(150) NOT NULL,
+salary Double NOT NULL,
+start Date NOT NULL,
+primary key (id)
+);
+describe employee_payroll;
+
+
+
+
 
